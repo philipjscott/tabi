6589a3596ea8db28a1b9a1e269250d87fb887443
Philip Scott <pscott@zeptohost.com>
2017-08-05 00:27:09 -0400

    Successfully got middleware working

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const phaserModule = path.join(__dirname, '../node_modules/phaser-ce/');
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+const pixi = path.join(phaserModule, 'build/custom/pixi.js');
+const p2 = path.join(phaserModule, 'build/custom/p2.js');
+
+module.exports = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/../src/entry.js')
+  ],
+  output: {
+    path: '/',
+    publicPath: 'http://localhost:8080/js',
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|mp3)$/,
+        use: [
+          'file-loader',
+
+        ]
+      },
+      { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
+      { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
+      { test: /p2\.js/, use: ['expose-loader?p2'] }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    alias: {
+      'phaser': phaser,
+      'pixi': pixi,
+      'p2': p2
+    }
+  },
+  // tut said to do this: google what tut does
+  target: 'web'
+};
